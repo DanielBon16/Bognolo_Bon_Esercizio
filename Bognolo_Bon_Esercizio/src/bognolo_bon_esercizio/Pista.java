bbdab14c96590ae56b087f38cf3f9b15fca121a2
DavideBognolo <davide.boglolo@isitgo.it>
2022-11-23 09:24:12 +0100

    Commento

--- Bognolo_Bon_Esercizio/src/bognolo_bon_esercizio/Pista.java
+++ Bognolo_Bon_Esercizio/src/bognolo_bon_esercizio/Pista.java
@@ -4,10 +4,51 @@
  */
 package bognolo_bon_esercizio;
 
-/**
- *
- * @author user
- */
 public class Pista {
+    protected int numero;
+    protected String condizioni;
+    protected double lunghezza;
+    protected int nCorsie;
+    public Pista(){}
+    public Pista(int numero, String condizioni, double lunghezza, int nCorsie)
+    {
+        this.condizioni=condizioni;
+        this.lunghezza=lunghezza;
+        this.nCorsie=nCorsie;
+        this.numero=numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getCondizioni() {
+        return condizioni;
+    }
+
+    public void setCondizioni(String condizioni) {
+        this.condizioni = condizioni;
+    }
+
+    public double getLunghezza() {
+        return lunghezza;
+    }
+
+    public void setLunghezza(double lunghezza) {
+        this.lunghezza = lunghezza;
+    }
+
+    public int getnCorsie() {
+        return nCorsie;
+    }
+
+    public void setnCorsie(int nCorsie) {
+        this.nCorsie = nCorsie;
+    }
     
+    //qwertyuiop
 }
