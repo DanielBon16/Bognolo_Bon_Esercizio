ccdd6b06a7d8e1fe329d4a82c09e14a97d2e4b9e
DavideBognolo <davide.boglolo@isitgo.it>
2022-11-23 08:54:09 +0100

    inizio

---
+++ Bognolo_Bon_Esercizio/src/bognolo_bon_esercizio/Bognolo_Bon_Esercizio.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bognolo_bon_esercizio;
+
+/**
+ *
+ * @author user
+ */
+public class Bognolo_Bon_Esercizio {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
